9f3b17a630a21bd7d6a4b1d105f12078f01bb30a
TrungDucMai <trungmaiduc196@gmail.com>
2021-10-24 23:20:30 +0700

    t2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {MyserviceService} from "./myservice.service";
+import {MyserviceService} from "./service/myservice.service";
 import { NewCmpComponent } from './new-cmp/new-cmp.component';
 import { TimelinesComponent } from './timelines/timelines.component';
 import { RouterModule, Routes } from '@angular/router'
+import {AppRoutingModule} from "./app-routing.module";
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NewCmpComponent,
-    TimelinesComponent
+    TimelinesComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    AppRoutingModule
   ],
   providers: [MyserviceService],
   bootstrap: [AppComponent]
