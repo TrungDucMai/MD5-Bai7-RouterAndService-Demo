344a38139702a65b8f55cfc3aa7cf1c4a8b8d8e8
TrungDucMai <trungmaiduc196@gmail.com>
2021-10-24 21:25:38 +0700

    t

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MyserviceService} from "./myservice.service";
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+import { RouterModule, Routes } from '@angular/router'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewCmpComponent,
+    TimelinesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([])
   ],
-  providers: [],
+  providers: [MyserviceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
